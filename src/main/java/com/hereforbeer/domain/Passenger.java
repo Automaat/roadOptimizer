61985f0cb59746930ead7a8336b6d1213136cfb0
pkruczek <piotrek628@gmail.com>
2016-06-04 12:54:54 +0200

    added mongo property

--- src/main/java/com/hereforbeer/domain/Passenger.java
+++ src/main/java/com/hereforbeer/domain/Passenger.java
@@ -1,27 +1,20 @@
 package com.hereforbeer.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.geo.Point;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.UUID;
 
 @Data
-@Builder(builderMethodName = "hiddenEventBuilder")
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(exclude = {"id"})
-@Document(collection = "passengers")
 public class Passenger {
-    private String id;
 
     private String firstname;
 
     private String lastname;
 
     private Point point;
-
-    public static PassengerBuilder builder(){
-        return hiddenEventBuilder().id(UUID.randomUUID().toString());
-    }
 }
--- src/main/java/com/hereforbeer/repositories/UserRepository.java
+++ src/main/java/com/hereforbeer/repositories/UserRepository.java
@@ -1,15 +1,9 @@
 package com.hereforbeer.repositories;
 
+import com.hereforbeer.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import com.hereforbeer.domain.User;
-
-import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findOneById(String id);
-    Optional<User> findOneByNick(String id);
-    Optional<User> findOneByFirstname(String id);
-    Optional<User> findOneByLastname(String id);
+public interface UserRepository extends MongoRepository<User, String>{
 }
