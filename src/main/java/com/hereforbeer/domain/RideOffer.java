6f2d43b5129210085b03f381e90f61d5bdeb20b9
Automaat <marcin0skalski@gmail.com>
2016-06-04 12:41:32 +0200

    added rideOffer entity

---
+++ src/main/java/com/hereforbeer/domain/RideOffer.java
@@ -0,0 +1,31 @@
+package com.hereforbeer.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"id"})
+@Builder(builderMethodName = "hiddenBuilder")
+@Document(collection = "rideOffers")
+public class RideOffer {
+
+    @Id
+    private String id = UUID.randomUUID().toString();
+
+    private LocalDate rideDate;
+    private Point start;
+    private Point end;
+    private int sits;
+
+    public static RideOfferBuilder builder() {
+        return hiddenBuilder().id(UUID.randomUUID().toString());
+    }
+
+}
