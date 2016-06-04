9b71d6984b60a0deddf08ca92262702a3eb5c6fd
Automaat <marcin0skalski@gmail.com>
2016-06-04 15:01:05 +0200

    added exceptions to user log in

--- src/main/java/com/hereforbeer/services/UserService.java
+++ src/main/java/com/hereforbeer/services/UserService.java
@@ -2,12 +2,16 @@ package com.hereforbeer.services;
 
 import com.hereforbeer.domain.User;
 import com.hereforbeer.repositories.UserRepository;
+import com.hereforbeer.web.BadRequestException;
 import com.hereforbeer.web.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+import static com.hereforbeer.web.ErrorInfo.USER_DUPLICATED;
+import static com.hereforbeer.web.ErrorInfo.USER_NOT_FOUND;
+import static com.hereforbeer.web.ErrorInfo.WRONG_PASSWORD;
 import static com.hereforbeer.web.dto.DTOMapper.parseUserFromDTO;
 
 @Service
@@ -23,7 +27,7 @@ public class UserService {
     public void createUser(UserDTO userDTO) {
         User user = parseUserFromDTO(userDTO);
         userRepository.findOneByNick(user.getNick()).ifPresent((u) -> {
-            throw new RuntimeException();
+            throw new BadRequestException(USER_DUPLICATED);
         });
 
         userRepository.save(user);
@@ -32,12 +36,12 @@ public class UserService {
     public void login(String nick, String password) {
         Optional<User> userFromDB = userRepository.findOneByNick(nick);
 
-        userFromDB.orElseThrow(RuntimeException::new);
+        userFromDB.orElseThrow(() -> new BadRequestException(USER_NOT_FOUND));
 
         userFromDB
                 .ifPresent(u -> {
                     if (!u.getPassword().equals(password)) {
-                        throw new RuntimeException();
+                        throw new BadRequestException(WRONG_PASSWORD);
                     }
                 });
     }
